2d54a5f41b0385c357e8cf21e53f270d0977fcb5
Nikolaos Dimopoulos <nikos@niden.net>
2018-04-30 16:50:52 -0400

    Added ErrorHandler Provider cest

---
+++ tests/unit/library/Providers/ErrorHandlerCest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Niden\Tests\unit\library\Providers;
+
+use function date_default_timezone_get;
+use Niden\Providers\ConfigProvider;
+use Niden\Providers\ErrorHandlerProvider;
+use Niden\Providers\LoggerProvider;
+use Phalcon\Di\FactoryDefault;
+use \UnitTester;
+
+class ErrorHandlerCest
+{
+    /**
+     * @param UnitTester $I
+     */
+    public function checkRegistration(UnitTester $I)
+    {
+        $diContainer = new FactoryDefault();
+        $provider    = new ConfigProvider();
+        $provider->register($diContainer);
+        $provider    = new LoggerProvider();
+        $provider->register($diContainer);
+        $provider    = new ErrorHandlerProvider();
+        $provider->register($diContainer);
+
+        $config = $diContainer->getShared('config');
+
+        $I->assertEquals(date_default_timezone_get(), $config->path('app.timezone'));
+        $I->assertEquals(ini_get('display_errors'), $config->path('app.devMode'));
+        $I->assertEquals(E_ALL, ini_get('error_reporting'));
+    }
+}

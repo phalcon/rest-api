95f9ec71d05f7485bdad3b05739ecab4b46f7e37
Nikolaos Dimopoulos <nikos@niden.net>
2018-05-31 16:50:17 -0400

    Refactoring the login

--- api/controllers/LoginController.php
+++ api/controllers/LoginController.php
@@ -6,6 +6,7 @@ use function explode;
 use Lcobucci\JWT\Builder;
 use Niden\Exception\ModelException;
 use Niden\Http\Response;
+use Niden\Models\Users;
 use Niden\Traits\UserTrait;
 use Phalcon\Filter;
 use Phalcon\Mvc\Controller;
@@ -39,28 +40,51 @@ class LoginController extends Controller
             'Incorrect credentials'
         );
 
-        $builder = new Builder();
-        $token   = $builder
-                        ->setIssuer('http://phalconphp.com')
-                        ->setAudience($user->get('usr_domain_name'))
-                        ->setId($user->get('usr_token_id'), true)
-                        ->setIssuedAt(time())
-                        ->setNotBefore(time() + 60)
-                        ->setExpiration(time() + 3600)
-                        ->getToken();
+        $token = $this->getToken($user);
 
-        $stringToken = $token->__toString();
-        list($pre, $mid, $post) = explode('.', $stringToken);
-        $result = $user
-                    ->set('usr_token_pre', $pre)
-                    ->set('usr_token_mid', $mid)
-                    ->set('usr_token_post', $post)
-                    ->save();
-        $this->checkResult($result, 'Cannot update user record');
+        $this->updateRecord($user, $token);
 
         /**
          * User found - Return token
          */
-        $this->response->setPayloadSuccess(['token' => $stringToken]);
+        $this->response->setPayloadSuccess(['token' => $token]);
+    }
+
+    /**
+     * @param Users $user
+     *
+     * @return string
+     * @throws ModelException
+     */
+    private function getToken(Users $user): string
+    {
+        $builder = new Builder();
+        $token   = $builder
+            ->setIssuer('http://phalconphp.com')
+            ->setAudience($user->get('usr_domain_name'))
+            ->setId($user->get('usr_token_id'), true)
+            ->setIssuedAt(time())
+            ->setNotBefore(time() + 60)
+            ->setExpiration(time() + 3600)
+            ->getToken();
+
+        return $token->__toString();
+    }
+
+    /**
+     * @param Users  $user
+     * @param string $token
+     *
+     * @throws ModelException
+     */
+    private function updateRecord(Users $user, string $token)
+    {
+        list($pre, $mid, $post) = explode('.', $token);
+        $result = $user
+            ->set('usr_token_pre', $pre)
+            ->set('usr_token_mid', $mid)
+            ->set('usr_token_post', $post)
+            ->save();
+        $this->checkResult($result, 'Cannot update user record');
     }
 }

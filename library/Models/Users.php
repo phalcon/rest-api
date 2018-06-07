ec421484bea3b88c29f3cd8aeb3d70fc64c49f74
Nikolaos Dimopoulos <nikos@niden.net>
2018-06-07 13:00:58 -0400

    Removed token fields from the model

--- library/Models/Users.php
+++ library/Models/Users.php
@@ -34,9 +34,6 @@ class Users extends AbstractModel
             'usr_password'       => Filter::FILTER_STRING,
             'usr_domain_name'    => Filter::FILTER_STRING,
             'usr_token_password' => Filter::FILTER_STRING,
-            'usr_token_pre'      => Filter::FILTER_STRING,
-            'usr_token_mid'      => Filter::FILTER_STRING,
-            'usr_token_post'     => Filter::FILTER_STRING,
             'usr_token_id'       => Filter::FILTER_STRING,
         ];
     }

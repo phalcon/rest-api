827b733dd681c3738fc7ac0afe1a87bc7eb5f3ed
Nikolaos Dimopoulos <nikos@niden.net>
2018-07-17 19:30:31 -0400

    Added relationship constants

---
+++ library/Constants/Relationships.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Niden\Constants;
+
+class Relationships
+{
+    const COMPANY         = 'company';
+    const INDIVIDUAL      = 'individual';
+    const INDIVIDUAL_TYPE = 'individualType';
+    const INDIVIDUALS     = 'individuals';
+    const PRODUCT         = 'product';
+    const PRODUCT_TYPE    = 'productType';
+}
---
+++ tests/unit/library/Constants/RelationshipsCest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Niden\Tests\unit\library\Constants;
+
+use \CliTester;
+use Niden\Constants\Relationships;
+
+class RelationshipsCest
+{
+    public function checkConstants(CliTester $I)
+    {
+        $I->assertEquals('company', Relationships::COMPANY);
+        $I->assertEquals('individual', Relationships::INDIVIDUAL);
+        $I->assertEquals('individualType', Relationships::INDIVIDUAL_TYPE);
+        $I->assertEquals('individuals', Relationships::INDIVIDUALS);
+        $I->assertEquals('product', Relationships::PRODUCT);
+        $I->assertEquals('productType', Relationships::PRODUCT_TYPE);
+    }
+}

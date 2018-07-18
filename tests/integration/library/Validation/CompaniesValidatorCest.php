184afe13b1fcfb266b8040e5d32436e8d3d33369
Nikolaos Dimopoulos <nikos@niden.net>
2018-07-18 10:59:10 -0400

    Added validator for company records

---
+++ tests/integration/library/Validation/CompaniesValidatorCest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Niden\Tests\integration\library\Transformers;
+
+use IntegrationTester;
+use Niden\Validation\CompaniesValidator;
+
+class CompaniesValidatorCest
+{
+    /**
+     * @param IntegrationTester $I
+     *
+     * @throws \Niden\Exception\ModelException
+     */
+    public function checkTransformer(IntegrationTester $I)
+    {
+        $validation = new CompaniesValidator();
+        $_POST     = [
+            'name'    => '',
+            'address' => '123 Phalcon way',
+            'city'    => 'World',
+            'phone'   => '555-999-4444',
+        ];
+        $messages = $validation->validate($_POST);
+        $I->assertEquals(1, count($messages));
+        $I->assertEquals('The company name is required', $messages[0]->getMessage());
+    }
+}

ff76e718fe58c34cfea9993608823d14e813dd01
Nikolaos Dimopoulos <nikos@niden.net>
2018-08-03 17:29:44 -0400

    Removed references to the events manager

--- api/config/providers.php
+++ api/config/providers.php
@@ -8,7 +8,6 @@ use Niden\Providers\CacheDataProvider;
 use Niden\Providers\ConfigProvider;
 use Niden\Providers\DatabaseProvider;
 use Niden\Providers\ErrorHandlerProvider;
-use Niden\Providers\EventsManagerProvider;
 use Niden\Providers\LoggerProvider;
 use Niden\Providers\RequestProvider;
 use Niden\Providers\ResponseProvider;
--- cli/config/providers.php
+++ cli/config/providers.php
@@ -9,7 +9,6 @@ use Niden\Providers\CliDispatcherProvider;
 use Niden\Providers\ConfigProvider;
 use Niden\Providers\DatabaseProvider;
 use Niden\Providers\ErrorHandlerProvider;
-use Niden\Providers\EventsManagerProvider;
 use Niden\Providers\LoggerProvider;
 
 return [

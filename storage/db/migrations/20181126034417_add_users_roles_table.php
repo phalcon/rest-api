74fdd42a8f14ec312106b2e6d6030d154aeb153e
kaioken <max@mctekk.com>
2018-11-30 22:33:16 -0400

    fix migrations

--- storage/db/migrations/20181126034417_add_users_roles_table.php
+++ storage/db/migrations/20181126034417_add_users_roles_table.php
@@ -1,19 +1,20 @@
-<?php
+    <?php
 
-use Phinx\Migration\AbstractMigration;
-use Phinx\Db\Adapter\MysqlAdapter;
+    use Phinx\Migration\AbstractMigration;
+    use Phinx\Db\Adapter\MysqlAdapter;
 
-class AddUsersRolesTable extends AbstractMigration
-{
-    public function change()
+    class AddUsersRolesTable extends AbstractMigration
     {
-        $table = $this->table("user_roles", ['id' => false, 'primary_key' => ["users_id", "apps_id", "company_id"], 'engine' => "InnoDB", 'encoding' => "utf8", 'collation' => "utf8_general_mysql500_ci", 'comment' => "", 'row_format' => "Dynamic"]);
-        $table->addColumn('users_id', 'integer', ['null' => false, 'limit' => MysqlAdapter::INT_REGULAR, 'precision' => 10])
-            ->addColumn('apps_id', 'integer', ['null' => false, 'limit' => MysqlAdapter::INT_REGULAR, 'precision' => 10, 'after' => 'users_id'])
-            ->addColumn('company_id', 'integer', ['null' => false, 'limit' => MysqlAdapter::INT_REGULAR, 'precision' => 10, 'after' => 'apps_id'])
-            ->addColumn('created_at', 'datetime', ['null' => false, 'after' => 'company_id'])
-            ->addColumn('updated_at', 'datetime', ['null' => true, 'after' => 'created_at'])
-            ->addColumn('is_deleted', 'boolean', ['null' => true, 'limit' => MysqlAdapter::INT_TINY, 'precision' => 3, 'after' => 'updated_at'])
-            ->save();
+        public function change()
+        {
+            $table = $this->table('user_roles', ['id' => false, 'primary_key' => ['users_id', 'apps_id', 'company_id'], 'engine' => 'InnoDB', 'encoding' => 'utf8mb4', 'collation' => 'utf8mb4_unicode_ci', 'comment' => '', 'row_format' => 'Dynamic']);
+            $table->addColumn('users_id', 'integer', ['null' => false, 'limit' => MysqlAdapter::INT_REGULAR, 'precision' => 10])
+                ->addColumn('roles_id', 'integer', ['null' => false, 'limit' => MysqlAdapter::INT_REGULAR, 'precision' => 10, 'after' => 'users_id'])
+                ->addColumn('apps_id', 'integer', ['null' => false, 'limit' => MysqlAdapter::INT_REGULAR, 'precision' => 10, 'after' => 'roles_id'])
+                ->addColumn('company_id', 'integer', ['null' => false, 'limit' => MysqlAdapter::INT_REGULAR, 'precision' => 10, 'after' => 'apps_id'])
+                ->addColumn('created_at', 'datetime', ['null' => false, 'after' => 'company_id'])
+                ->addColumn('updated_at', 'datetime', ['null' => true, 'after' => 'created_at'])
+                ->addColumn('is_deleted', 'boolean', ['null' => false, 'default' => '0', 'limit' => MysqlAdapter::INT_TINY, 'precision' => 3, 'after' => 'updated_at'])
+                ->save();
+        }
     }
-}

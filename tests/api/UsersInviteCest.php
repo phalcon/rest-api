268f8beb24e5634da16505dd792d829534080a5b
Rafael White <rwhite@mctekk.com>
2018-12-11 09:41:57 -0400

    Change test users invite

--- tests/api/UsersInviteCest.php
+++ tests/api/UsersInviteCest.php
@@ -18,37 +18,45 @@ class UsersInviteCest
         $data = json_decode($response, true);
 
         $I->assertTrue($data['email'] == $testEmail);
-    }
 
-    public function processUserInvite(ApiTester $I):void
-    {
-        $userData = $I->apiLogin();
-        $testEmail = 'testMC@example.com';
+        $hash = $data['invite_hash'];
 
-        //Buscar email hash en la base de datos poner luego en url.
+        $I->sendPost(`/v1/users/invite/insert?hash=$hash`, [
+            'firstname' => 'testFirstName',
+            'lastname' => 'testLastName',
+            'displayname' => 'testDisplayName',
+            'password' => 'testpassword',
+        ]);
 
-        $I->haveHttpHeader('Authorization', $userData->token);
-
-        $I->sendGet(`/v1/users-invite`);
         $I->seeResponseIsSuccessful();
         $response = $I->grabResponse();
-        $data = json_decode($response, true);
+        $dataInvite = json_decode($response, true);
 
-        $I->assertTrue($data['email'] == $testEmail);
-
-        // $hash =  $data['invite_hash'];
-
-        // $I->sendPost(`/v1/users/invite/insert?hash=$hash`, [
-        //     'firstname' => 'testFirstName',
-        //     'lastname' => 'testLastName',
-        //     'displayname' => 'testDisplayName',
-        //     'password' => 'testpassword',
-        // ]);
-
-        // $I->seeResponseIsSuccessful();
-        // $response = $I->grabResponse();
-        // $data = json_decode($response, true);
-
-        // $I->assertTrue($data['email'] == $testEmail);
+        $I->assertTrue($dataInvite['email'] == $testEmail);
     }
+
+    // public function processUserInvite(ApiTester $I):void
+    // {
+    //     $userData = $I->apiLogin();
+    //     $testEmail = 'testMC@example.com';
+
+    //     //Buscar email hash en la base de datos poner luego en url.
+
+    //     $I->haveHttpHeader('Authorization', $userData->token);
+
+    //     $hash = $data['invite_hash'];
+
+    //     $I->sendPost(`/v1/users/invite/insert?hash=$hash`, [
+    //         'firstname' => 'testFirstName',
+    //         'lastname' => 'testLastName',
+    //         'displayname' => 'testDisplayName',
+    //         'password' => 'testpassword',
+    //     ]);
+
+    //     $I->seeResponseIsSuccessful();
+    //     $response = $I->grabResponse();
+    //     $data = json_decode($response, true);
+
+    //     $I->assertTrue($data['email'] == $testEmail);
+    // }
 }
